0d8b8cb44ad456390d2f0ba1108487997e8dc343
Saladin21 <13519187@std.stei.itb.ac.id>
2022-04-10 00:13:09 +0700

    add game over zen and wave mode

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -4,14 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;       
-    public float restartDelay = 5f;            
+    public PlayerHealth playerHealth;
+    public GameObject gameOverPanel;
+    //public float restartDelay = 5f;            
 
 
     Animator anim;                          
-    float restartTimer;
+    //float restartTimer;
 
     public Text warningText;
+    bool isGameOver = false;
 
     void Awake()
     {
@@ -21,31 +23,31 @@ public class GameOverManager : MonoBehaviour
 
     void Update()
     {
-        if (playerHealth.currentHealth <= 0)
+        if (playerHealth.currentHealth <= 0 && !isGameOver)
         {
             anim.SetTrigger("GameOver");
+            isGameOver = true;
 
             //Stoptime
             ScoreManager scoreManager = ScoreManager.Instance;
             scoreManager.StopTime();
 
-            restartTimer += Time.deltaTime;
 
-            if (restartTimer >= restartDelay)
+            //Save score
+            if (scoreManager.isZenMode)
             {
-                //Save score
-                if (scoreManager.isZenMode)
-                {
-                    scoreManager.AddScore(new Score(ScoreManager.playerName, ScoreManager.stopWatch.currentTime));
-                }
-                else
-                {
-                    scoreManager.AddScore(new Score(ScoreManager.playerName, ScoreManager.score, WaveManager.currentWave), false);
-                }
-                scoreManager.SaveScore();
-
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                scoreManager.AddScore(new Score(ScoreManager.playerName, ScoreManager.stopWatch.currentTime));
+                gameOverPanel.transform.Find("Score").GetComponent<Text>().text = "Time: " + ScoreManager.stopWatch.currentTime.ToString();
             }
+            else
+            {
+                scoreManager.AddScore(new Score(ScoreManager.playerName, ScoreManager.score, WaveManager.currentWave), false);
+                gameOverPanel.transform.Find("Score").GetComponent<Text>().text = "Score: " +ScoreManager.score.ToString();
+            }
+            scoreManager.SaveScore();
+            //gameOverPanel.SetActive(true);
+
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
